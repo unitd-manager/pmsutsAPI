0d90e01e590a90e95767d750db632eacf8b91eb9
Sabina <sabina@unitdtechnologie.com>
2024-04-10 19:02:54 +0530

    leave

--- routes/leave.js
+++ routes/leave.js
@@ -58,6 +58,50 @@ app.use(fileUpload({
       }
     );
   });
+  
+  app.get('/getDashboardLeave', (req, res, next) => {
+    db.query(`SELECT l.status
+    ,l.leave_id
+    ,e.employee_id
+    ,l.leave_type
+    ,l.no_of_days
+    ,l.reason
+    ,l.from_date
+    ,l.to_date
+    ,l.no_of_days_next_month
+    ,l.employee_id
+    ,l.date
+    ,COUNT(CASE WHEN l.leave_type = 'permission' THEN 1 END) AS permission_count
+    ,COUNT(CASE WHEN l.leave_type != 'permission' THEN 1 END) AS other_leave_count
+    ,CONCAT_WS(' ', e.first_name, e.last_name) AS employee_name
+    ,j.designation
+    ,e.citizen
+    FROM empleave l
+    LEFT JOIN employee e ON (l.employee_id = e.employee_id)
+    LEFT JOIN job_information j ON (j.employee_id = l.employee_id)
+    WHERE l.leave_id !=''
+    GROUP BY e.employee_id`,
+      (err, result) => {
+         
+        if (err) {
+          console.log('error: ', err)
+          return res.status(400).send({
+            data: err,
+            msg: 'failed',
+          })
+        } else {
+          return res.status(200).send({
+            data: result,
+            msg: 'Success',
+              });
+        
+        
+  
+          }
+   
+      }
+    );
+  });
 app.post('/getLeave', (req, res, next) => {
     db.query(`SELECT l.status
     ,l.leave_id
@@ -134,6 +178,151 @@ app.post('/getLeave', (req, res, next) => {
       }
     );
   });
+  
+app.post('/getAppLeave', (req, res, next) => {
+    const userEmail = req.body.email; // Assuming the email is sent in the request body
+
+    db.query(`
+        SELECT 
+            l.employee_id,
+            e.first_name,
+            e.employee_id,
+            e.email,
+            SUM(CASE WHEN YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalLeaveThisYear,
+            SUM(CASE WHEN YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+                     WHEN YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+                     ELSE 0 END) AS TotalLeaveThisMonth,
+            SUM(CASE WHEN l.leave_type = 'permission' THEN l.no_of_days ELSE 0 END) AS TotalPermission,
+            SUM(CASE WHEN l.leave_type = 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalPermissionThisYear,
+            SUM(CASE WHEN l.leave_type = 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+                     WHEN l.leave_type = 'permission' AND YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+                     ELSE 0 END) AS TotalPermissionThisMonth,
+            SUM(CASE WHEN l.leave_type != 'permission' THEN l.no_of_days ELSE 0 END) AS TotalLeave,
+            SUM(CASE WHEN l.leave_type != 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalLeaveExceptPermissionThisYear,
+            SUM(CASE WHEN l.leave_type != 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+                     WHEN l.leave_type != 'permission' AND YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+                     ELSE 0 END) AS TotalLeaveExceptPermissionThisMonth
+        FROM 
+            empleave l
+        LEFT JOIN 
+            employee e ON l.employee_id = e.employee_id
+        WHERE 
+            e.email = ?
+    `, [userEmail], (err, result) => {
+        if (err) {
+            console.log('error: ', err)
+            return res.status(400).send({
+                data: err,
+                msg: 'failed',
+            })
+        } else {
+            return res.status(200).send({
+                data: result[0],
+                msg: 'Success',
+            });
+        }
+    });
+});
+
+  
+ app.get('/getLeavePermission', (req, res, next) => {
+    db.query(`SELECT 
+    l.employee_id,
+    e.first_name,
+    e.employee_id,
+    SUM(CASE WHEN YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalLeaveThisYear,
+    SUM(CASE WHEN YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             WHEN YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             ELSE 0 END) AS TotalLeaveThisMonth,
+    SUM(CASE WHEN l.leave_type = 'permission' THEN l.no_of_days ELSE 0 END) AS TotalPermission,
+    SUM(CASE WHEN l.leave_type = 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalPermissionThisYear,
+    SUM(CASE WHEN l.leave_type = 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             WHEN l.leave_type = 'permission' AND YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             ELSE 0 END) AS TotalPermissionThisMonth,
+    SUM(CASE WHEN l.leave_type != 'permission' THEN l.no_of_days ELSE 0 END) AS TotalLeave,
+    SUM(CASE WHEN l.leave_type != 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalLeaveExceptPermissionThisYear,
+    SUM(CASE WHEN l.leave_type != 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             WHEN l.leave_type != 'permission' AND YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             ELSE 0 END) AS TotalLeaveExceptPermissionThisMonth
+FROM 
+    empleave l
+    LEFT JOIN employee e ON l.employee_id=e.employee_id
+WHERE 
+    l.employee_id !='' 
+ 
+   GROUP BY l.employee_id`,
+      (err, result) => {
+         
+        if (err) {
+          console.log('error: ', err)
+          return res.status(400).send({
+            data: err,
+            msg: 'failed',
+          })
+        } else {
+          return res.status(200).send({
+            data: result,
+            msg: 'Success',
+              });
+        
+        
+  
+          }
+   
+      }
+    );
+  });
+
+
+app.post('/getLeaveandPermission', (req, res, next) => {
+  
+    db.query(`SELECT 
+    l.employee_id,
+    e.first_name,
+    e.employee_id,
+    SUM(CASE WHEN YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalLeaveThisYear,
+    SUM(CASE WHEN YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             WHEN YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             ELSE 0 END) AS TotalLeaveThisMonth,
+    SUM(CASE WHEN l.leave_type = 'permission' THEN l.no_of_days ELSE 0 END) AS TotalPermission,
+    SUM(CASE WHEN l.leave_type = 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalPermissionThisYear,
+    SUM(CASE WHEN l.leave_type = 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             WHEN l.leave_type = 'permission' AND YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             ELSE 0 END) AS TotalPermissionThisMonth,
+    SUM(CASE WHEN l.leave_type != 'permission' THEN l.no_of_days ELSE 0 END) AS TotalLeave,
+    SUM(CASE WHEN l.leave_type != 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalLeaveExceptPermissionThisYear,
+    SUM(CASE WHEN l.leave_type != 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             WHEN l.leave_type != 'permission' AND YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             ELSE 0 END) AS TotalLeaveExceptPermissionThisMonth
+FROM 
+    empleave l
+    LEFT JOIN employee e ON l.employee_id=e.employee_id
+WHERE 
+    e.email =${db.escape(req.body.email)}
+ 
+   GROUP BY l.employee_id`,
+      (err, result) => {
+         
+        if (err) {
+          console.log('error: ', err)
+          return res.status(400).send({
+            data: err,
+            msg: 'failed',
+          })
+        } else {
+          return res.status(200).send({
+            data: result,
+            msg: 'Success',
+              });
+        
+        
+  
+          }
+   
+      }
+    );
+  });
+
 
  app.post('/getEmployeeEmail', (req, res, next) => {
     db.query(`SELECT 
@@ -347,15 +536,24 @@ GROUP BY
              app.post('/getTotalPastLeaveHistoryById', (req, res, next) => {
 db.query(`SELECT 
     l.employee_id,
-    SUM(l.no_of_days) AS TotalLeave,
     SUM(CASE WHEN YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalLeaveThisYear,
     SUM(CASE WHEN YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
              WHEN YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
-             ELSE 0 END) AS TotalLeaveThisMonth
+             ELSE 0 END) AS TotalLeaveThisMonth,
+    SUM(CASE WHEN l.leave_type = 'permission' THEN l.no_of_days ELSE 0 END) AS TotalPermission,
+    SUM(CASE WHEN l.leave_type = 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalPermissionThisYear,
+    SUM(CASE WHEN l.leave_type = 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             WHEN l.leave_type = 'permission' AND YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             ELSE 0 END) AS TotalPermissionThisMonth,
+    SUM(CASE WHEN l.leave_type != 'permission' THEN l.no_of_days ELSE 0 END) AS TotalLeave,
+    SUM(CASE WHEN l.leave_type != 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) THEN l.no_of_days ELSE 0 END) AS TotalLeaveExceptPermissionThisYear,
+    SUM(CASE WHEN l.leave_type != 'permission' AND YEAR(l.from_date) = YEAR(CURRENT_DATE()) AND MONTH(l.from_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             WHEN l.leave_type != 'permission' AND YEAR(l.to_date) = YEAR(CURRENT_DATE()) AND MONTH(l.to_date) = MONTH(CURRENT_DATE()) THEN l.no_of_days
+             ELSE 0 END) AS TotalLeaveExceptPermissionThisMonth
 FROM 
     empleave l
 WHERE 
-    l.employee_id = ${db.escape(req.body.employee_id)}
+    l.employee_id =  ${db.escape(req.body.employee_id)}
 GROUP BY 
     l.employee_id`,
                   (err, result) => {
@@ -386,4 +584,3 @@ GROUP BY
   });
   
   module.exports = app;
-  
